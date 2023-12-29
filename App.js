d69b87f50b7d2a632c6c8870d2d5059bef1098c5
Vikrant-1 <vikrantkainth090699@gmail.com>
2023-12-29 20:13:22 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: "center",backgroundColor:'white' }} >
+      <Text style={{fontSize:22,color:'black'}} >
+        App center build
+      </Text>
+    </View>
+  )
+}
+export default App;
